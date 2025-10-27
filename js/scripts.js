51e12498653d48dbc1aa93a21561831cada818ec
Aidan Walny <222332094+aidan-walny@users.noreply.github.com>
2025-10-27 18:54:16 -0400

    Index: Changed the heading.
    JS:
    Added a function using the built in Math function to randomly pick a number 0-2 and assign that number to an choice in the game.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,21 @@
 // console.log("Hello Wolrd.")
 
-function getComputerChoice ()
+function getComputerChoice () {
+    let computer_choice = Math.floor(Math.random() * 3)
+
+    console.log(computer_choice)
+
+    if (computer_choice == 0) {
+        let computer_move = "rock"
+        console.log(computer_move)
+    }
+    else if (computer_choice == 1) {
+        let computer_move = "paper"
+        console.log(computer_move)
+    }
+    else if (computer_choice == 2) {
+        let computer_move = "scissors"
+        console.log(computer_move)
+    }
+}
+getComputerChoice()
