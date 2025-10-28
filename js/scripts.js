3ba4c457d4214303dbdc6514bb31c09873a7e11c
Aidan Walny <222332094+aidan-walny@users.noreply.github.com>
2025-10-28 19:03:25 -0400

    JS:
    Cleaning up code.

--- js/scripts.js
+++ js/scripts.js
@@ -38,70 +38,28 @@ function playGame() {
         }
     }
 }
-playGame() 
-
-// Global variables to keep score.
-// let humanScore = 0
-// let computerScore = 0
-
-// Function to play a single round.
-// function playRound(humanChoice, computerChoice) {
-//     // Making human string lowercase.
-//     let accurate_humanChoice = humanChoice.toLowerCase();
-//     // console.log(accurate_humanChoice)
-//     console.log(accurate_humanChoice, computerChoice)
-
-//     // Game logic
-//     if (accurate_humanChoice == 'rock' & computerChoice == 'rock' || accurate_humanChoice == 'paper' & computerChoice == 'paper' || accurate_humanChoice == 'scissors' & computerChoice == 'scissors') {
-//         return `Draw, you both selected ${accurate_humanChoice}.`;
-//     }
-//     else if (accurate_humanChoice == 'rock' & computerChoice == 'scissors' || accurate_humanChoice == 'paper' & computerChoice == 'rock' || accurate_humanChoice == 'scissors' & computerChoice == 'paper') {
-//         humanScore += 1
-//         return `You win, ${accurate_humanChoice} beats ${computerChoice}!`;
-//     }
-//     else if (accurate_humanChoice == 'rock' & computerChoice == 'paper' || accurate_humanChoice == 'paper' & computerChoice == 'scissors' || accurate_humanChoice == 'scissors' & computerChoice == 'rock') {
-//         computerScore += 1
-//         return `You lose, ${computerChoice} beats ${accurate_humanChoice}!`;
-//     }
-// }
-
-// const humanSelection = getHumanChoice();
-// const computerSelection = getComputerChoice();
-
-// outcome = playRound(humanSelection, computerSelection);
-// console.log(outcome)
-// console.log(`Human Score:${humanScore}`)
-// console.log(`Computer Score:${computerScore}`)
+playGame()
 
 // Function to obtain a computer choice in the game using the Math function.
 function getComputerChoice() {
     let computer_choice = Math.floor(Math.random() * 3);
-    // console.log(computer_choice)
 
     if (computer_choice == 0) {
         let computer_move = "rock";
-        // console.log(computer_move)
         return computer_move;
     }
     else if (computer_choice == 1) {
         let computer_move = "paper";
-        // console.log(computer_move)
         return computer_move;
     }
     else if (computer_choice == 2) {
         let computer_move = "scissors";
-        // console.log(computer_move)
         return computer_move;
     }
 }
-// computerSelection = getComputerChoice();
-// console.log(computerSelection)
 
 // Function to obtain a human choice with a prompt.
 function getHumanChoice() {
     let human_choice = prompt("Your choice:");
-    // console.log(human_choice)
     return human_choice;
 }
-// humanSelection = getHumanChoice();
-// console.log(humanSelection)
